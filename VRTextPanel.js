7bbc2ad266c88fe1d30d27cae451360089dac987
Bhautik Joshi <bjoshi+github@gmail.com>
2019-05-28 22:21:14 -0700

    compartment

--- VRTextPanel.js
+++ VRTextPanel.js
@@ -15,16 +15,20 @@
 	OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-THREE.TextPanel = function(initText, colorString){
-	colorString = typeof colorString !== 'undefined' ? colorString : "rgba(255,0,0,0.95)";
+THREE.TextPanel = function(initTextArray, colorString){
+	this.txSize = 14;
+	this.txHeight = this.txSize  + 4;
 	this.canvas1 = document.createElement('canvas');
+	this.canvas1.width = 256;
+	this.canvas1.height = 64;
 	this.context1 = this.canvas1.getContext('2d');
-	this.context1.font = "Bold 40px Arial";
+	//this.context1.font = "Bold 32px Arial";
+	this.context1.font = this.txSize + "px/1.4 arial, sans-serif";
 	this.context1.fillStyle = colorString;
-	this.context1.fillText(initText, 0, 50);
-	
+		
 	this.texture1 = new THREE.Texture(this.canvas1)
-	this.texture1.needsUpdate = true;
+
+	this.RenderText(initTextArray);
 
 	this.material1 = new THREE.MeshBasicMaterial( {map: this.texture1, side:THREE.DoubleSide } );
 	this.material1.transparent = true;
@@ -33,34 +37,22 @@ THREE.TextPanel = function(initText, colorString){
 		// size: 1m width
 	    new THREE.PlaneGeometry(1.0,this.canvas1.height/this.canvas1.width),
 	    this.material1
-	  );
+	);
 	this.mesh1.rotateY(Math.PI);
 };
 
 THREE.TextPanel.prototype.constructor = THREE.TextPanel
 
-// var canvas1 = document.createElement('canvas');
-// var context1 = canvas1.getContext('2d');
-// context1.font = "Bold 40px Arial";
-// context1.fillStyle = "rgba(255,0,0,0.95)";
-// context1.fillText('ZAKZAK!', 0, 50);
-//
-// // canvas contents will be used for a texture
-// var texture1 = new THREE.Texture(canvas1)
-// texture1.needsUpdate = true;
-//
-// var material1 = new THREE.MeshBasicMaterial( {map: texture1, side:THREE.DoubleSide } );
-// material1.transparent = true;
-//
-// var mesh1 = new THREE.Mesh(
-// 	// size: 1m width
-//     new THREE.PlaneGeometry(1.0,canvas1.height/canvas1.width),
-//     material1
-//   );
-//   mesh1.rotateY(Math.PI);
-// mesh1.position.set(0,0,
-//
-// THREE.TextPanel.prototype.Init = function(text) {
-// 	var scope = THREE.TextPanel;
-// 	console.log(text);
-// }
+THREE.TextPanel.prototype.RenderText = function(textArray) {
+	this.context1.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
+	for (var i = 0; i < textArray.length; i++) {
+		this.context1.fillText(textArray[i], 0, this.txHeight + i*this.txHeight);
+	}
+	this.texture1.needsUpdate = true;
+}
+
+THREE.TextPanel.prototype.GetMesh = function(text) {
+	return this.mesh1;
+}
+
+
